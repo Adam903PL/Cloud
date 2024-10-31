a850fe18a9932b0312ea1413e3b091a5d6fab25c
Adam Pukaluk <pukaluk.adam505@gmail.com>
2024-10-31 18:27:45 +0100

    version 1.8.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 
 import Documents from "./Documents";
+import FolderPage from "./FolderPage";
+import { BrowserRouter,Routes,Route } from "react-router-dom"
 function App() {
 
 
  
   return (
       <>
-        <Documents/>
+        <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Documents/>}/>
+            <Route path="/folder" element={<FolderPage/>}/>
+           </Routes>
+        </BrowserRouter>
+        </div>
 
       </>
   );
---
+++ src/FolderPage.tsx
@@ -0,0 +1,10 @@
+function FolderPage(){
+
+    return(
+        <>
+            <h1>Welcom at FolderPage</h1>
+        </>
+    )
+}
+
+export default FolderPage
